1694de3c168d89f698408d8a70ab67d01ed5ee37
PiriyevRufat <tu7pj36at@code.edu.az>
2024-04-02 09:18:00 +0400

    task weapon

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp12
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           bool exit = false;
+            Weapon weapon = new Weapon(30,10,false);
+            bool run = false;
+            while (run) 
+            {
+                Console.WriteLine("0: Informasiya almaq ucun");
+                Console.WriteLine("1: Ates acmaq ucun");
+                Console.WriteLine("2:Getremainbulletcount metodu ucun");
+                Console.WriteLine("3:reload metodu ucun");
+                Console.WriteLine("4:changefire metodu ucun");
+                Console.WriteLine("5:proqrami dayandir");
+                string input= Console.ReadLine();
+               
+                switch (input) 
+                {
+                    case "0":
+                        Console.WriteLine("informasiya almaq");
+                      
+                        break;
+                    case "1":
+                            break; 
+                    case "2":
+                        Console.WriteLine("Getremainbulletcount");
+                        weapon.GetRemainBulletCount();
+
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Weapon
+    {
+        public byte Capacitybullet { get; set; }
+        public int Currentbullet { get; set; }
+        public bool Firemode { get; set; }
+        public Weapon(byte capacitybullet,int currentbullet,bool firemode) 
+        { 
+           Capacitybullet = capacitybullet;
+            Currentbullet = currentbullet;
+            Firemode = firemode;
+        }
+        public void Shoot() 
+        { 
+            if(Firemode==false) 
+            {
+               Currentbullet -= Currentbullet;
+                Console.WriteLine("tekli gulle atir");
+            }
+            else 
+            {
+                Console.WriteLine("gulle  ugurla atilir");
+            }
+        }
+        public int GetRemainBulletCount() 
+        {
+            return Capacitybullet- Currentbullet;
+        }
+
+        public int Reload() 
+        {
+            Currentbullet = Capacitybullet;
+            Console.WriteLine("Daragi yeniden doldur");
+            return Capacitybullet;
+        }
+
+        public  void ChangeFireMode(bool Firemode) 
+        { 
+            if (Firemode==false) 
+            {
+                Console.WriteLine("tekli gulle atilir");
+            }
+            else if (Firemode==true) 
+            {
+                Console.WriteLine("avtomatik gulle acilir");
+            }
+           
+            
+        }
+    }
+    
+}
